27236aa9a8671a94948ddc32e2568dbe83870236
shreykoyangodu <shreyakoyangodu@gmail.com>
2023-09-18 14:46:50 +0530

    sixth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const mongoose=require("mongoose");
+const route=require("./routes/route");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app=express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//DB connection
+mongoose
+      .connect('mongodb://127.0.0.1:27017/cybersecurity').then(()=>{
+      console.log("MongoDb is successfully connected")
+    })
+    .catch((err)=>{
+    console.log(err)
+     });
+
+
+app.listen(3500,()=>{
+    console.log("server is successfully connected at port 3500");
+});
